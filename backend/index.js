160d775f8988a4164353c0807d402b5e66e63fbe
kundankumaryadav88 <karanjashveersingh88@gmail.com>
2025-06-04 22:49:10 +0530

    packend_updated

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,34 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+require('dotenv').config();
+
 const connectDB = require('./src/config/db');
 const authRoutes = require("./src/routes/authRoutes");
 const taskRoutes = require("./src/routes/taskRoutes");
 
-require('dotenv').config();
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
-
-
-//DBConnection
+// DB Connection
 connectDB();
+
+// Middlewares
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
+app.use(mongoSanitize());
+app.use(xss());
 
-//routes
+// Routes
 app.use("/api", authRoutes);
-app.use("/api/tasks", taskRoutes)
+app.use("/api/tasks", taskRoutes);
 
-app.listen(PORT, () =>{
-    console.log(`Server Running on port ${PORT}`)
-})
-
-
+// Server
+app.listen(PORT, () => {
+  console.log(`Server Running on port ${PORT}`);
+});
